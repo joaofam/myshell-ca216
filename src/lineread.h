18fb5b15375a9e28995c3ef26b34962d92f1278a
Joao Pereira <joao.pereira2@mail.dcu.ie>
2021-03-24 00:42:11 +0000

    lineread added

--- src/lineread.h
+++ src/lineread.h
@@ -1,3 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+char *line_read(void)
+{
+    char *line = NULL;
+    size_t len = 0;
 
-/* batch - https://pages.cs.wisc.edu/~remzi/Classes/537/Spring2012/Projects/p2a.html */
+    if(getline(&line, &len, stdin)== -1)
+    {
+        if(feof(stdin))
+        {
+            exit(EXIT_SUCCESS);
+        }
+        else 
+        { 
+            printf("error occured reading line");
+            exit(EXIT_FAILURE);
+        }
+    }
+    return line;
+}
